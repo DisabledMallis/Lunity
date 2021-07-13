ff9d68d3e87846a194ee8b57a378a531ebd0d393
Gamerclient28921 <gamersqaud@hotmail.com>
2021-07-13 15:57:52 +0200

    some Actor offsets and Vector3F

--- Lunity/Client/Bridge/Actor.h
+++ Lunity/Client/Bridge/Actor.h
@@ -5,9 +5,20 @@
 struct Actor : DynamicStruct {
     Actor() : Actor("Actor") {};
     Actor(std::string structName) : DynamicStruct(structName, 0) {
+		this->addField(new DynamicField("fallDistance", 0x1DC));
         this->addField(new DynamicField("onGround", 0x1E0));
+		this->addField(new DynamicField("pos1", 0x4D0));
+		this->addField(new DynamicField("pos2", 0x4DC));
+		this->addField(new DynamicField("hitbox", 0x4EC));
+		this->addField(new DynamicField("velocity", 0x50C));
     };
-
+	//fallDistance it's become read only in this struct/class
+	auto getFallDistance() -> float {
+		DynamicField* theField = (DynamicField*)this->get("fallDistance");
+		bool* theVal = (bool*)theField->asVoid();
+		return *theVal;
+	}
+	
 	auto isOnGround() -> bool {
 		DynamicField* theField = (DynamicField*)this->get("onGround");
 		bool* theVal = (bool*)theField->asVoid();
@@ -17,6 +28,50 @@ struct Actor : DynamicStruct {
 		DynamicField* theField = (DynamicField*)this->get("onGround");
 		bool* theVal = (bool*)theField->asVoid();
 		*theVal = onGround;
+	}
+	//hitbox
+	auto getHitbox() -> Vector2F {
+		DynamicField* theField = (DynamicField*)this->get("hitbox");
+        Vector2F* hitbox = ((Vector2F*)theField->asVoid());
+        return *hitbox;
+	}
+	void setHitbox(Vector2F newHitbox){
+		DynamicField* theField = (DynamicField*)this->get("hitbox");
+        Vector2F* hitbox = ((Vector2F*)theField->asVoid());
+		*hitbox = newHitbox;
+	}
+	//pos1
+	auto getPosLower() -> Vector3F {
+		DynamicField* theField = (DynamicField*)this->get("pos1");
+        Vector3F* pos = ((Vector3F*)theField->asVoid());
+        return *pos;
+	}
+	void setPosLower(Vector3F newPos){
+		DynamicField* theField = (DynamicField*)this->get("pos1");
+        Vector3F* pos = ((Vector3F*)theField->asVoid());
+		*pos = newPos;
+	}
+	//pos2
+	auto getPosUpper() -> Vector3F {
+		DynamicField* theField = (DynamicField*)this->get("pos2");
+        Vector3F* pos = ((Vector3F*)theField->asVoid());
+        return *pos;
+	}
+	void setPosUpper(Vector3F newPos){
+		DynamicField* theField = (DynamicField*)this->get("pos2");
+        Vector3F* pos = ((Vector3F*)theField->asVoid());
+		*pos = newPos;
+	}
+	//velocity
+	auto getVelocity() -> Vector3F {
+		DynamicField* theField = (DynamicField*)this->get("velocity");
+        Vector3F* vel = ((Vector3F*)theField->asVoid());
+        return *vel;
+	}
+	void setVelocity(Vector3F newVel){
+		DynamicField* theField = (DynamicField*)this->get("velocity");
+        Vector3F* vel = ((Vector3F*)theField->asVoid());
+		*vel = newVel;
 	}
 };
 
--- Lunity/Utils/Math.h
+++ Lunity/Utils/Math.h
@@ -34,4 +34,37 @@ struct Vector2S : public Vector2<short> {
     Vector2S(short x, short y) : Vector2<short>(x, y) {};
 };
 
+template <typename T>
+struct Vector3 {
+	union {
+		struct {
+			T x, y, z;
+		};
+		T arr[3];
+	};
+
+	Vector3<T>() { x = y = z = 0; };
+	Vector3<T>(T x, T y, T z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	Vector3<T> operator+(Vector3<T> toAdd) const {
+        T newX = x + toAdd.x;
+        T newY = y + toAdd.y;
+		T newZ = z + toAdd.z;
+        return Vector3<T>(newX, newY, newZ);
+    }
+    Vector3<T> operator-(Vector3<T> toSub) const {
+        T newX = x - toSub.x;
+        T newY = y - toSub.y;
+		T newZ = z - toSub.z;
+        return Vector3<T>(newX, newY, newZ);
+    }
+};
+
+struct Vector3F : public Vector3<float> {
+    
+};
+
 #endif /* LUNITY_UTILS_MATH */
